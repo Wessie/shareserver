b704c6dabfe9eff1b3fae518382c891a147be2ce
wessie <github@wessie.info>
2015-08-17 02:24:57 +0200

    Added Save methods to User and File structs

--- database.go
+++ database.go
@@ -15,9 +15,14 @@ Database layout:
 
 users:
 	<username>:
-		password: <hash>
-		profile: <json> // see UserProfile
-		files: <hashes>
+		password: <password-hash>
+		profile: <userprofile:json> // see UserProfile
+		files: [<filehash>, ...]
+
+files:
+	<filehash>:
+		path: <filepath>
+		info: <fileinfo:json>
 
 */
 
@@ -33,6 +38,7 @@ var (
 
 	fileBucket  = []byte("files")
 	filePathKey = []byte("path")
+	fileInfoKey = []byte("info")
 )
 
 type Database struct {
@@ -119,6 +125,7 @@ type User struct {
 
 	UserProfile
 
+	// bolt is the boltdb associated with this user
 	bolt *bolt.DB
 }
 
@@ -152,16 +159,96 @@ func (u *User) SetPassword(current, new string) bool {
 	return true
 }
 
+func (u User) Save() error {
+	if len(u.pwd) == 0 {
+		return errors.New("invalid password: empty")
+	}
+	if len(u.Name) == 0 {
+		return errors.New("invalid username: empty")
+	}
+	if u.bolt == nil {
+		return errors.New("invalid user instance: no database")
+	}
+
+	return u.bolt.Update(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucketIfNotExists(userBucket)
+		if err != nil {
+			// TODO: error clarification
+			return err
+		}
+
+		userdb, err := b.CreateBucketIfNotExists([]byte(u.Name))
+		if err != nil {
+			// TODO: error clarification
+			return err
+		}
+
+		// Store the users password
+		if err := userdb.Put(userPasswordKey, u.pwd); err != nil {
+			return errors.New("failed to store password: " + err.Error())
+		}
+
+		// Store the users profile
+		userProfile, err := json.Marshal(u.UserProfile)
+		if err != nil {
+			return errors.New("failed to marshal userprofile: " + err.Error())
+		}
+
+		if err := userdb.Put(userProfileBucket, userProfile); err != nil {
+			return errors.New("failed to store userprofile: " + err.Error())
+		}
+
+		return nil
+	})
+}
+
 // UserProfile contains non-critical information about a User
 type UserProfile struct{}
 
 type File struct {
+	// Hash generated from file contents
 	Hash []byte
+	// Path is the filepath to the file on-disk
 	Path string
 
 	FileInfo
 
+	// bolt is the boltdb associated with this file
 	bolt *bolt.DB
 }
 
-type FileInfo struct{}
+type FileInfo struct {
+	// Filename is the filename as given by uploader
+	Filename string
+}
+
+func (f File) Save() error {
+	return f.bolt.Update(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucketIfNotExists(fileBucket)
+		if err != nil {
+			return err
+		}
+
+		filedb, err := b.CreateBucketIfNotExists(f.Hash)
+		if err != nil {
+			return err
+		}
+
+		// Store the files on-disk path
+		if err := filedb.Put(filePathKey, []byte(f.Path)); err != nil {
+			return err
+		}
+
+		// Store the files info
+		fileInfo, err := json.Marshal(f.FileInfo)
+		if err != nil {
+			return err
+		}
+
+		if err := filedb.Put(fileInfoKey, fileInfo); err != nil {
+			return err
+		}
+
+		return nil
+	})
+}
